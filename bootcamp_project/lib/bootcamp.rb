235d47552079b79e90177cfbf2fbd3c4395f3e6a
hien nguyen <thuhiennguyen198@gmail.com>
2021-06-22 20:17:16 -0400

    update

--- bootcamp_project/lib/bootcamp.rb
+++ bootcamp_project/lib/bootcamp.rb
@@ -30,19 +30,41 @@ class Bootcamp
   end
 
   def enroll(student)
-    if students.size < student_capacity.size
-      students << student
+    if @students.length < @student_capacity
+      @students << student
       return true
     else
-      false
+      return false
     end
   end
 
-#   def enrolled?(student)
-#     puts student.enroll
-#   end
+  def enrolled?(student)
+    @students.map.any?{|enrolled_student| enrolled_student.include?(student)}
+  end
 
-#   def self.student_to_teacher_ratio(students, teachers)
-#     puts (students.length / teachers.length).floor(1)
-#   end
+  def student_to_teacher_ratio
+    @students.length / @teachers.length
+  end
+
+  def add_grade(student, grade)
+    if self.enrolled?(student) == true
+      @grades[student] << grade
+      return true
+    else
+      return false
+    end
+  end
+
+  def num_grades(student)
+    @grades[student].length
+  end
+
+  def average_grade(student)
+    if @grades[student].empty? || self.enrolled?(student) == false
+      return nil
+    else
+      return (@grades[student]).sum / @grades[student].length
+    end
+  end
+
 end
--- monkey_patching_project/lib/array.rb
+++ monkey_patching_project/lib/array.rb
@@ -1,4 +1,75 @@
 # Monkey-Patch Ruby's existing Array class to add your own custom methods
 class Array
-  
+    def span
+        return nil if self.empty?
+        self.max - self.min  
+    end
+
+
+    def average
+        return nil if self.empty?
+        self.sum / (self.length * 1.0)
+    end
+
+
+
+    def median
+        return nil if self.empty?
+        sorted = self.sort
+        if self.length.odd?
+            i = (self.length - 1) / 2
+            return sorted[i]
+        else
+            x = self.length / 2
+            return (sorted[x] + sorted[x - 1]) / 2.0
+        end
+    end
+
+    def counts
+        hash = Hash.new(0)
+            self.each {|ele| hash[ele] += 1}
+        return hash
+    end
+
+    def my_count(value)
+        hash = Hash.new(0)
+            self.each {|ele| hash[ele] += 1}
+        if hash.has_key?(value)
+            return hash[value]
+        else
+            return 0
+        end
+    end
+
+    def my_index(value)
+        self.each_with_index do |ele, i|
+            if value == ele 
+                return i 
+            end        
+        end
+        return nil
+    end
+
+    def my_uniq
+        hash = Hash.new(0)
+        self.each {|ele| hash[ele] += 1}
+        return hash.keys
+    end
+
+
+    def my_transpose
+        h = self.length 
+        w = self[0].length
+        result = Array.new(h){Array.new(w)}
+
+        (0...h).each do |row|
+            (0...w).each do |col|
+                result[row][col] = self[col][row]
+            end
+        end
+
+        return result
+    end
+
+
 end
